45b1c850177d374c7547b72789b201d2ff28ad4c
Ankit Kumar Rathod <ar16052@gmail.com>
2018-10-29 19:13:57 +0530

    create the base test file

---
+++ DjangoTestCases/testapp/test/test_base.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from testapp.models import Band, Member
+
+
+class BaseTestCase(TestCase):
+    def setUp(self):
+        self.address_one = 'India'
+        self.name_one = 'Mahaveer'
+
+        self.address_two = 'Japan'
+        self.name_two = 'Rajveer'
+
+        self.member_name = 'Robbert'
+        self.member_contact = 9589555555
+
+        self.band_one = Band.objects.create(name=self.name_one, address=self.address_one)
+        self.band_two = Band.objects.create(name=self.name_two, address=self.address_two)
+
+        self.member = Member.objects.create(name=self.member_name ,contact=self.member_contact, band=self.band_one)

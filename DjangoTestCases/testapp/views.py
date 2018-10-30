aab9575862ee31a87badce99e7ea899eb2793ff6
Ankit Kumar Rathod <ar16052@gmail.com>
2018-10-30 15:18:27 +0530

    add views

--- DjangoTestCases/testapp/views.py
+++ DjangoTestCases/testapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Band
 # Create your views here.
+
+
+def index(request):
+    bands = Band.objects.all()
+    context = {'bands' : bands}
+    return render(request, 'testapp/index.html', context)
